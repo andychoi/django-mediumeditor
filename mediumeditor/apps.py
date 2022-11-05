527eef6c48e7923187ad66c5c8cb68bc30683863
Andy Choi <3507699+andychoi@users.noreply.github.com>
2022-11-04 22:26:21 -0700

    Update apps.py

--- mediumeditor/apps.py
+++ mediumeditor/apps.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 
 class MediumEditorConfig(AppConfig):
